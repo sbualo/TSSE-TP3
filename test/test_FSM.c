c6c1437e3f90800384b869cc71e878f2ea141f71
sbualo <sbualo@itba.edu.ar>
2024-04-04 16:47:28 -0300

    Se eliminan comentarion innecesarios y codigo muerto

--- test/test_FSM.c
+++ test/test_FSM.c
@@ -64,8 +64,7 @@ void test_lectura_tarjeta_invalida_en_avance_FSM_a_estado_validando_tarjeta(void
 
 void test_reset_fsm_a_estado_inicial(void) {
     TestState = estado_validando_tarjeta;
-    TestState = fsm(TestState,
-                    TIMEOUT_DEFAULT); // Con este evento TestState debe de quedar en el mismo lugar
+    TestState = fsm(TestState, TIMEOUT_DEFAULT);
     TEST_ASSERT_EQUAL(estado_puerta_cerrada, TestState);
 }
 
@@ -76,33 +75,27 @@ void test_reset_fsm_desde_todos_los_estados_hacia_estado_inicial(void) {
     TEST_ASSERT_EQUAL(estado_puerta_cerrada, TestState);
 
     TestState = estado_validando_tarjeta;
-    TestState = fsm(TestState,
-                    TIMEOUT_DEFAULT); // Con este evento TestState debe de quedar en el mismo lugar
+    TestState = fsm(TestState, TIMEOUT_DEFAULT);
     TEST_ASSERT_EQUAL(estado_puerta_cerrada, TestState);
 
     TestState = estado_ingreso_primer_numero;
-    TestState = fsm(TestState,
-                    TIMEOUT_DEFAULT); // Con este evento TestState debe de quedar en el mismo lugar
+    TestState = fsm(TestState, TIMEOUT_DEFAULT);
     TEST_ASSERT_EQUAL(estado_puerta_cerrada, TestState);
 
     TestState = estado_ingreso_segundo_numero;
-    TestState = fsm(TestState,
-                    TIMEOUT_DEFAULT); // Con este evento TestState debe de quedar en el mismo lugar
+    TestState = fsm(TestState, TIMEOUT_DEFAULT);
     TEST_ASSERT_EQUAL(estado_puerta_cerrada, TestState);
 
     TestState = estado_ingreso_tercer_numero;
-    TestState = fsm(TestState,
-                    TIMEOUT_DEFAULT); // Con este evento TestState debe de quedar en el mismo lugar
+    TestState = fsm(TestState, TIMEOUT_DEFAULT);
     TEST_ASSERT_EQUAL(estado_puerta_cerrada, TestState);
 
     TestState = estado_ingreso_cuarto_numero;
-    TestState = fsm(TestState,
-                    TIMEOUT_DEFAULT); // Con este evento TestState debe de quedar en el mismo lugar
+    TestState = fsm(TestState, TIMEOUT_DEFAULT);
     TEST_ASSERT_EQUAL(estado_puerta_cerrada, TestState);
 
     TestState = estado_validando_pin;
-    TestState = fsm(TestState,
-                    TIMEOUT_DEFAULT); // Con este evento TestState debe de quedar en el mismo lugar
+    TestState = fsm(TestState, TIMEOUT_DEFAULT);
     TEST_ASSERT_EQUAL(estado_puerta_cerrada, TestState);
     LED_OPEN_DOOR_CMockIgnore();
     TestState = estado_puerta_abierta;
@@ -171,14 +164,12 @@ void test_estado_ingreso_pin_incorrecto(void) {
 void test_avance_FSM_de_estado_estado_validando_pin_a_estado_puerta_abierta(void) {
     TestState = estado_validando_pin;
     LED_OPEN_DOOR_Ignore();
-    TestState = fsm(TestState,
-                    PIN_VALIDO); // Con este evento la FSM avanza a ingreso_primer_numero
+    TestState = fsm(TestState, PIN_VALIDO);
     TEST_ASSERT_EQUAL(estado_puerta_abierta, TestState);
 }
 void test_avance_FSM_de_estado_estado_validando_pin_a_estado_ingreso_primer_numero(void) {
     TestState = estado_validando_pin;
-    TestState = fsm(TestState,
-                    PIN_INVALIDO); // Con este evento la FSM avanza a ingreso_primer_numero
+    TestState = fsm(TestState, PIN_INVALIDO);
     TEST_ASSERT_EQUAL(estado_ingreso_primer_numero, TestState);
 }
 
@@ -294,148 +285,3 @@ void test_funcion_generador_evento_fin_tabla(void) {
     eventos TestEvent = get_event();
     TEST_ASSERT_EQUAL(FIN_TABLA, TestEvent);
 }
-
-// typedef enum {
-//     LECTURA_TARJETA,
-//     TARJETA_VALIDA,
-//     TARJETA_INVALIDA,
-//     LECTURA_NUMERO_TECLADO,
-//     PIN_VALIDO,
-//     PIN_INVALIDO,
-//     TIMEOUT_DEFAULT,
-//     TIMEOUT_PUERTA_ABIERTA,
-//     FIN_TABLA
-// } eventos;
-
-// #include "unity.h"
-
-// #include "leds.h"
-
-// #define TEST_LED_TO_BIT(x) (1 << (x - 1))
-// #define TEST_ALL_LED_OFF   0x00
-// #define TEST_ALL_LED_ON    0b1111111111111111
-
-// static uint16_t leds_virtuales;
-// static const int LED = 4;
-
-// /**
-//  * @brief Funcion que se ejecuta antes de cada test (nombre especifico de ceedling)
-//  *
-//  */
-// void setUp(void) {
-
-//     leds_init(&leds_virtuales);
-// }
-
-// void test_todos_los_led_deben_estar_apagados_al_iniciar_el_driver(void) {
-//     uint16_t leds_virtuales = 0xFF;
-//     leds_init(&leds_virtuales);
-//     TEST_ASSERT_EQUAL_UINT16(0x00, leds_virtuales);
-// }
-
-// /* prueba de encendido de un led*/
-
-// void test_prender_un_led(void) {
-
-//     leds_turn_on(LED);
-//     // El bit 2 esta en alto
-//     TEST_ASSERT_EQUAL_UINT16(1 << (LED - 1), leds_virtuales);
-//     // Todos los bits estan en bajo
-//     TEST_ASSERT_BITS_LOW(~(1 << (LED - 1)), leds_virtuales);
-// }
-
-// void test_apagar_un_led(void) {
-
-//     leds_turn_on(LED);
-//     leds_turn_off(LED);
-
-//     TEST_ASSERT_EQUAL_UINT16(0x00, leds_virtuales);
-// }
-
-// void test_operar_varios_led(void) {
-
-//     leds_turn_on(5);
-//     leds_turn_on(7);
-//     leds_turn_on(5);
-//     leds_turn_off(5);
-//     leds_turn_off(9);
-
-//     TEST_ASSERT_EQUAL_UINT16(1 << (7 - 1), leds_virtuales);
-// }
-
-// void test_prender_todos_los_led(void) {
-
-//     leds_turn_all_on();
-
-//     TEST_ASSERT_EQUAL_UINT16(TEST_ALL_LED_ON, leds_virtuales);
-// }
-
-// void test_apagar_todos_los_led(void) {
-//     leds_turn_all_off();
-//     TEST_ASSERT_EQUAL_UINT16(TEST_ALL_LED_OFF, leds_virtuales);
-// }
-
-// void test_limit_values(void) {
-//     leds_turn_on(0);
-//     TEST_ASSERT_EQUAL_UINT16(TEST_ALL_LED_OFF, leds_virtuales);
-//     leds_turn_on(1);
-//     TEST_ASSERT_EQUAL_UINT16(TEST_LED_TO_BIT(1), leds_virtuales);
-
-//     leds_turn_off(0);
-//     TEST_ASSERT_EQUAL_UINT16(TEST_LED_TO_BIT(1), leds_virtuales);
-//     leds_turn_off(1);
-//     TEST_ASSERT_EQUAL_UINT16(TEST_ALL_LED_OFF, leds_virtuales);
-
-//     // test valores superiores
-
-//     leds_turn_on(17);
-//     TEST_ASSERT_EQUAL_UINT16(TEST_ALL_LED_OFF, leds_virtuales);
-//     leds_turn_on(16);
-//     TEST_ASSERT_EQUAL_UINT16(TEST_LED_TO_BIT(16), leds_virtuales);
-
-//     leds_turn_off(17);
-//     TEST_ASSERT_EQUAL_UINT16(TEST_LED_TO_BIT(16), leds_virtuales);
-//     leds_turn_off(16);
-//     TEST_ASSERT_EQUAL_UINT16(TEST_ALL_LED_OFF, leds_virtuales);
-// }
-
-// void test_limit_values_all_leds_on(void) {
-
-//     leds_turn_all_on();
-//     TEST_ASSERT_EQUAL_UINT16(TEST_ALL_LED_ON, leds_virtuales);
-//     // Ya estan todos los led prendidos
-
-//     leds_turn_on(0);
-//     TEST_ASSERT_EQUAL_UINT16(TEST_ALL_LED_ON, leds_virtuales);
-//     leds_turn_on(1);
-//     TEST_ASSERT_EQUAL_UINT16(TEST_ALL_LED_ON, leds_virtuales);
-
-//     leds_turn_off(0);
-//     TEST_ASSERT_EQUAL_UINT16(TEST_ALL_LED_ON, leds_virtuales);
-//     leds_turn_off(1);
-//     TEST_ASSERT_EQUAL_UINT16(0b1111111111111110, leds_virtuales);
-
-//     leds_turn_on(1);
-//     // test valores superiores
-
-//     leds_turn_on(17);
-//     TEST_ASSERT_EQUAL_UINT16(TEST_ALL_LED_ON, leds_virtuales);
-//     leds_turn_on(16);
-//     TEST_ASSERT_EQUAL_UINT16(TEST_ALL_LED_ON, leds_virtuales);
-
-//     leds_turn_off(17);
-//     TEST_ASSERT_EQUAL_UINT16(TEST_ALL_LED_ON, leds_virtuales);
-//     leds_turn_off(16);
-//     TEST_ASSERT_EQUAL_UINT16(0b0111111111111111, leds_virtuales);
-// }
-
-// void test_check_led_status_off(void) {
-//     bool status = led_check_status(1);
-//     TEST_ASSERT_EQUAL_UINT16(false, status);
-// }
-
-// void test_check_led_status_on(void) {
-//     leds_turn_on(1);
-//     bool status = led_check_status(1);
-//     TEST_ASSERT_EQUAL_UINT16(true, status);
-// }
